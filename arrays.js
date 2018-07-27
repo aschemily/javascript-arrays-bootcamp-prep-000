e38142828878718cfee85eb674515796cc4b8b9b
Emily Asch <e.asch@me.com>
2018-07-27 20:55:03 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,7 +4,7 @@ function addElementToBeginningOfArray (array,element){
  return [element,...array ]
 }
 
-function destructivelyRemoveElementFromBeginningOfArray (array,element){
+function destructivelyAddElementFromBeginningOfArray (array,element){
  array.unshift (element)
  return array
 }
